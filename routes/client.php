a4148dac09cb5b6cf6a962f8e97be2117942d871
Hatim Lagzar <lhatimdev@gmail.com>
2022-06-06 18:34:58 +0100

    constraint locale param in route

--- routes/client.php
+++ routes/client.php
@@ -38,7 +38,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('{locale?}')
+Route::prefix('{locale?}')->where(
+    ['locale' => 'en|fr|de|es|it'],
+)
      ->middleware([SetupLocaleMiddleware::class, SetDefaultLocaleForUrlsMiddleware::class])
      ->group(function () {
          Route::post('contact', ContactUsController::class)->name('contact');
