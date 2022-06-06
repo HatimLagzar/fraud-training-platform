17bcb246c8f0428ca7fe0e1304af928d9b1ceab5
sssimo2018@gmail.com <sssimo2018@gmail.com>
2022-06-06 19:31:07 +0000

    fixtypo

--- resources/views/mails/verification.blade.php
+++ resources/views/mails/verification.blade.php
@@ -6,8 +6,7 @@
 # {{__('Email Verification')}}
 
 {{__('Hi')}} {{ $user->name }},
-{{__('Please verify your account by clicking on the link below.
-')}}
+{{__('Please verify your account by clicking on the link below.')}}
 @component('mail::button', ['url' => route('verification.verify', ['id' => $user->getId(), 'hash' => $user->getVerificationToken()])])
 	{{__('Click Here')}}
 @endcomponent
